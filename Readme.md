5e762ae7bc38db3a5fc9f96c7b5f902e9141de62
Dmytro Denysov <dmytro.denysov@danads.se>
2023-03-22 18:44:14 +0200

    Initial commit

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-1. Possibility to override port in app
-2. Possibility to override git clone
+1. +Possibility to override port in app
+2. +Possibility to override git clone
 3. Possibility to on/off git clone
-4. Configure queues
+4. +Configure queues
--- images/base/start.sh
+++ images/base/start.sh
@@ -7,18 +7,21 @@ git clone "${APP_REPO}" .
 echo "Install dependencies"
 composer install
 
+if [ "${APP_DB}" = true ]
+then
 echo "Create database if exist"
 php bin/console doctrine:database:create --if-not-exists
 
 echo "Migrate database"
 php bin/console doctrine:migrations:migrate --no-interaction
+fi
 
 if [ "${APP_MODE}" = app ]
 then
-echo "symfony server:stop && symfony server:start --port=${APP_PORT}"
+symfony server:stop && symfony server:start --port="${APP_PORT}"
 fi
 
 if [ "${APP_MODE}" = supervisor ]
 then
-echo "supervisord -c /var/www/etc/supervisor/conf/messenger-worker.conf"
+supervisord -c /etc/supervisor/conf/messenger-worker.conf
 fi

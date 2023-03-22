db3fae0124947334947ee9650dbbfd92310c8ebf
Dmytro Denysov <dmytro.denysov@danads.se>
2023-03-22 17:48:29 +0200

    Initial commit

---
+++ images/app/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#  This script should be used for generating .env file for Laravel
+#
+touch "ENTRY_PORT_${APP_PORT}.txt"
+top
+#echo "Install dependencies"
+#composer install
+
+#echo "Create database if exist"
+#php bin/console doctrine:database:create --if-not-exists
+
+#echo "Migrate database"
+#php bin/console doctrine:migrations:migrate --no-interaction
+
--- images/base/start.sh
+++ images/base/start.sh
@@ -2,7 +2,9 @@
 #
 #  This script should be used for generating .env file for Laravel
 #
-echo "Install dapandencies"
+git clone "${APP_REPO}" .
+
+echo "Install dependencies"
 composer install
 
 echo "Create database if exist"
@@ -11,3 +13,12 @@ php bin/console doctrine:database:create --if-not-exists
 echo "Migrate database"
 php bin/console doctrine:migrations:migrate --no-interaction
 
+if [ "${APP_MODE}" = app ]
+then
+echo "symfony server:stop && symfony server:start --port=${APP_PORT}"
+fi
+
+if [ "${APP_MODE}" = supervisor ]
+then
+echo "supervisord -c /var/www/etc/supervisor/conf/messenger-worker.conf"
+fi
